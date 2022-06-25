bc7f6dfdcdeef36babfad7a6863616b758116b46
Andrey <abramov29.05@mail.ru>
2022-06-25 20:34:30 +0700

    Нахождения большего

---
+++ Zadacha_4/Program.cs
@@ -0,0 +1,29 @@
+﻿// Нахождения большеего между 2 3 7
+int a=2;
+int b=3;
+int c=7;
+int max= a;
+if(b>max) max=b;
+if(c>max) max=c;
+Console.Write("Max=");
+Console.WriteLine(max);
+
+// Нахождения большеего между 44 5 78
+int a1=44;
+int b1=5;
+int c1=78;
+int max1= a1;
+if(b1>max1) max1=b1;
+if(c1>max1) max1=c1;
+Console.Write("Max1=");
+Console.WriteLine(max1);
+
+// Нахождения большеего между 22 3 9
+int a2=22;
+int b2=3;
+int c2=9;
+int max2= a2;
+if(b2>max2) max2=b2;
+if(c2>max2) max2=c2;
+Console.Write("Max2=");
+Console.WriteLine(max2);
