2832b2c05c24ebdcf7dac9a6f92cc8497c925eeb
Andrey <abramov29.05@mail.ru>
2022-06-25 20:17:38 +0700

    Нахождения большего

---
+++ Zadacha_2/Program.cs
@@ -0,0 +1,20 @@
+﻿int a=5;
+int b=7;
+int max1= a;
+if(b>max1) max1=b;
+Console.Write("Max1=");
+Console.WriteLine(max1);
+
+int c=2;
+int d=10;
+int max2= c;
+if(d>max2) max2=d;
+Console.Write("Max2=");
+Console.WriteLine(max2);
+
+int e=-9;
+int f=-3;
+int max3= e;
+if(f>max3) max3=f;
+Console.Write("Max2=");
+Console.WriteLine(max3);
