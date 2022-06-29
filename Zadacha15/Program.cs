9e18f764838668f62c5603bf6ae6d714e1788d6c
Andrey <abramov29.05@mail.ru>
2022-06-29 17:03:49 +0700

    задача 15

--- Zadacha15/Program.cs
+++ Zadacha15/Program.cs
@@ -8,10 +8,10 @@ Console.WriteLine(dayNumber);                                  //Выводим 
 
 //____________________________________________________________________________
 
-Console.Write("Введите 3-х значное число: ");
+Console.Write("Введите 3-х значное число: ");                   
 int dayNumbers = Convert.ToInt32( Console.ReadLine());
-  if (dayNumbers %6==0 || dayNumbers  %7==0) {                   //Говорим что если рандомное число кратно 6 или 7 это выходной
-  Console.WriteLine("(этот день ВЫХОДНОЙ) да");                //Выводим "(этот день ВЫХОДНОЙ) да"
+  if (dayNumbers %6==0 || dayNumbers  %7==0) {                   
+  Console.WriteLine("(этот день ВЫХОДНОЙ) да");                
   }
-  else Console.WriteLine("(этот день НЕ выходной) нет");       //Иначе выводим это "(этот день НЕ выходной) нет"
+  else Console.WriteLine("(этот день НЕ выходной) нет");       
 
