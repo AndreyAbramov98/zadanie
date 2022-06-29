5317130c83015108041d6180ba329c075ee96156
Andrey <abramov29.05@mail.ru>
2022-06-29 20:50:33 +0700

    Как бы я не лопатил код , он не запускается (ничего не выдает , даже ошибку)

---
+++ Dop_zadanie/Program.cs
@@ -0,0 +1,11 @@
+﻿for (int day = 0,up=3, down=2, shest = 10; day == shest; day=day+up)
+{
+    if (day == 10)
+    
+    for (int i=day; i>shest; i=i-down)
+   
+       break;
+
+Console.WriteLine(day);
+}
+//Ничего не понятно и ничего не работает 
