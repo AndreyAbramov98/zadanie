d1df66b1a47d39fd22a2d8d82d3e68f4e4b00ae0
Andrey <abramov29.05@mail.ru>
2022-07-03 19:27:02 +0700

    Задача 19

---
+++ Zadacha19/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");                                 //Просим ввести число
+string? number = Console.ReadLine();                               //Вводим число
+
+void Numbers(string number)                                         //Из числа в строку
+{                                      
+  if (number[0]==number[4] || number[1]==number[3])                //Если 1 число =5 , а 2 ровно 4
+  {               
+    Console.WriteLine($"Ваше число: {number} - палиндром.");       //То "Ваше число: number - палиндром."
+  }
+  else Console.WriteLine($"Ваше число: {number} - НЕ палиндром."); //Иначе "Ваше число: number - НЕ палиндром."
+}
+
+if (number!.Length == 5)                                          //Проверяем какое это число (5-и значное?)
+{                                       
+  Numbers(number);                                                
+}
+else Console.WriteLine($"Введи правильное число(5-и значное)");  //Если нет то просим ввести правильное число
